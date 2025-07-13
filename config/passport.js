a24703b608b7fc54ddf291ffb80a22690e4073ed
Kunj Tyagi <110375352+Kunj-Tyagi@users.noreply.github.com>
2025-07-13 23:46:21 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+require("dotenv").config();
 
 passport.use(new GoogleStrategy({
     clientID: process.env.clientID,
